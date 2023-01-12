1c04c0747ceb82ce071e27261c0f6061067088c2
Ajay Chavan <ajayc9838@gmail.com>
2023-01-12 17:46:30 +0530

    changes done

--- APIGatewayService/src/main/java/com/niit/APIGatewayService/config/APIGateWayConfig.java
+++ APIGatewayService/src/main/java/com/niit/APIGatewayService/config/APIGateWayConfig.java
@@ -1,2 +1,20 @@
-package com.niit.APIGatewayService.config;public class APIGateWayConfig {
+package com.niit.APIGatewayService.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class APIGateWayConfig {
+
+    @Bean
+    public RouteLocator getRoutes(RouteLocatorBuilder builder) {
+
+        return builder.routes().route(p -> p.path("/api/v1/**").
+                        uri("lb://user-service/")).
+                route(p -> p.path("/api/v2/**").
+                        uri("lb://track-service/")).build();
+    }
+
 }
